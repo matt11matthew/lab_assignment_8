6bae3673dce21c8e2ad08b4a294c5d8c91290d9c
matt11matthew <matt11matthew@gmail.com>
2023-07-14 23:33:47 -0400

    Clean up

--- sort.c
+++ sort.c
@@ -56,25 +56,21 @@ void heapSort(int arr[], int n) {
 // extraMemoryAllocated counts bytes of extra memory allocated
 
 
-// Merge two subarrays L and M into arr
 void merge(int arr[], int l, int m, int r) {
     int i, j, k;
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    // Create temporary arrays
     int L[n1], R[n2];
 
-    // Copy data to temporary arrays L[] and R[]
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
 
-    // Merge the temporary arrays back into arr[l..r]
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
+    i = 0;
+    j = 0;
+    k = l;
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             arr[k] = L[i];
@@ -86,14 +82,11 @@ void merge(int arr[], int l, int m, int r) {
         k++;
     }
 
-    // Copy the remaining elements of L[], if there are any
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
-
-    // Copy the remaining elements of R[], if there are any
     while (j < n2) {
         arr[k] = R[j];
         j++;
@@ -103,10 +96,8 @@ void merge(int arr[], int l, int m, int r) {
 
 void mergeSort(int arr[], int l, int r) {
     if (l < r) {
-        // Same as (l+r)/2, but avoids overflow for large l and h
         int m = l + (r - l) / 2;
 
-        // Sort first and second halves
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
 
